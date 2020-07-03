db450d4625566eb969f4581c9cc3fbeb83b420f0
Ross Goodwin <ross.goodwin@gmail.com>
2020-07-03 01:26:53 -0700

    Create print📎.sh
    WARNING DANGER OF DOOM

---
+++ print📎.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# USAGE:
+# ./print📎.sh <📎_count> <outfile>
+#
+#     print📎.sh  Copyright (C) 2020  Ross Goodwin
+#    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+#    This is free software, and you are welcome to redistribute it
+#    under certain conditions; type `show c' for details.
+
+head -c ${1} < /dev/zero | tr '🌌' '📎' | dd of=${2}
